796b6c6a968ccec941725e630dadc209f53089bd
Darren Pinto <pintodarren999@gmail.com>
2025-04-11 18:07:47 -0500

    Readded optimizations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/robot/command/framework/commandtypes/SequentialCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/robot/command/framework/commandtypes/SequentialCommandGroup.java
@@ -32,7 +32,7 @@ public class SequentialCommandGroup extends CommandBase {
 
     @Override
     public void update() {
-        /*
+
         while (theCommandIndex < theCommands.length && theCurrentCommand.readyToExecute()) {
             // Update the current command
             theCurrentCommand.update();
@@ -58,9 +58,7 @@ public class SequentialCommandGroup extends CommandBase {
                 return;
             }
         }
-
-         */
-
+        /*
         if (!theCurrentCommand.readyToExecute()) {
             return;
         }
@@ -80,7 +78,7 @@ public class SequentialCommandGroup extends CommandBase {
             theCurrentCommand.initialize();
         }
 
-
+         */
     }
 
     @Override
